153bb6f00647397e8563108ebaa6fc954ca06eda
Dhruv Bhalani <156923442+dhruv-bhalani@users.noreply.github.com>
2024-01-23 19:10:55 +0530

    c language

---
+++ ch 5/1.c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+main()
+{
+	int a=15;
+	//	printf("enter number  :");
+	//	scanf("%d",&a);
+		printf("%d",++a);
+		printf("\n%d",--a);
+			
+}
---
+++ ch 5/2.c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+ int x,a=10,b=20;
+ clrscr();
+     printf("%d+%d=%d\n",a,b,a+b);
+    
+     printf("%d-%d=%d\n",a,b,b-a);
+
+     printf("%d*%d=%d\n",a,b,a*b);
+      
+     printf("%d/%d=%d\n",a,b,b/a);
+      
+     printf("%d%%%d=%d\n",a,b,b%a);
+ getch();    
+}
