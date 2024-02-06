4aa08d5e6cb67ab6ca94d1208648f4fd916bbe4c
Dhruv Bhalani <156923442+dhruv-bhalani@users.noreply.github.com>
2024-02-06 20:38:54 +0530

    Add files via upload

---
+++ CH-9/9.2/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int n=1;
+//	clrscr();
+	
+	do
+	{
+		printf("%d\t",n++);	
+	}while(n<=10);
+}
---
+++ CH-9/9.2/3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a=1,n;
+	clrscr();
+	printf("Enter any number : ");
+	scanf("%d",&n);
+	do
+	{
+		if(a%2==0)
+		{
+			printf("%d\t",a);
+	    }
+	    a++;
+	}while(a<=n);
+	getch();
+}
+
