75ab932f414242dfd4e53b0df77b1fd95b4bd4e0
Dhruv Bhalani <156923442+dhruv-bhalani@users.noreply.github.com>
2024-02-27 19:29:20 +0530

    Add files via upload

---
+++ ex=1/calculator.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#define p printf
+#define s scanf
+
+om()
+{
+
+    printf("\n1.press 1 for addition\n");
+    printf("2.press 2 for substraction\n");
+    printf("3.press 3 for multiplication\n");
+    printf("4.press 4 for division\n");
+    printf("5.press 5 for modulos\n");
+    printf("6.press 6 for exit program");
+}
+
+void addition(int a, int b)
+{
+    p("%d + %d = %d", a, b, a + b);
+}
+void substraction(int a, int b)
+{
+    p("%d - %d = %d", a, b, a - b);
+}
+void multiplication(int a, int b)
+{
+    p("%d * %d = %d", a, b, a * b);
+}
+void division(int a, int b)
+{
+    p("%d / %d = %d", a, b, a / b);
+}
+void modulos(int a, int b)
+{
+    p("%d %% %d = %d", a, b, a % b);
+}
+
+int main()
+{
+
+    int a, b, choice;
+    printf("enter any number: ");
+    scanf("%d", &a);
+    printf("enter any number: ");
+    scanf("%d", &b);
+
+jump:
+    om();
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+
+    case 1:
+        addition(a, b);
+        goto jump;
+    case 2:
+        substraction(a, b);
+        goto jump;
+    case 3:
+        multiplication(a, b);
+        goto jump;
+    case 4:
+        division(a, b);
+        goto jump;
+    case 5:
+        modulos(a, b);
+        goto jump;
+    case 6:
+        p("you are exited");
+        break;
+
+    default:
+        printf("unknown number!!");
+        goto jump;
+    }
+    return 0;
+}
+    
+    
---
+++ ex=1/calculator3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define p printf
+#define s scanf
+
+
+
+int main()
+{
+    int a, b, choice;
+    jump:
+    printf("1.press 1 for addition\n");
+    printf("2.press 2 for substraction\n");
+    printf("3.press 3 for multiplication\n");
+    printf("4.press 4 for division\n");
+    printf("5.press 5 for modulos\n");
+    printf("6.press 6 for exit program\n\n");
+    printf("enter any number: ");
+    scanf("%d", &a);
+    printf("enter any number: ");
+    scanf("%d\n\n", &b);
+    
+     
+    
+   
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+
+    case 1:
+        p("%d+%d=%d\n\n",a,b,a+b);
+        goto jump;
+    case 2:
+        p("%d-%d=%d\n\n",a,b,a-b);
+        goto jump;
+    case 3:
+        p("%d*%d=%d\n\n",a,b,a*b);
+        goto jump;
+    case 4:
+        p("%d/%d=%d\n\n",a,b,a/b);
+        goto jump;
+    case 5:
+        p("%d %% %d=%d\n\n",a,b,a%b);
+        goto jump;
+    case 6:
+        p("ghare ja\n");
+        break;
+
+    default:
+        printf("unknown number!!\n");
+        goto jump;
+    }
+    return 0;
+}
+    
+    
