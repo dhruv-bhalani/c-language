fa5c78b2927da3b9d358dadb81b55e14e9205e6b
Dhruv Bhalani <156923442+dhruv-bhalani@users.noreply.github.com>
2024-03-07 19:41:23 +0530

    c

---
+++ PR=6/1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+main()
+{
+	char name[20];
+	char rev[20];
+	int i,l,len,a=1;
+	printf("Enter Your Name : ");
+	scanf("%[^\n]",&name);
+	
+	for(i=0;name[i]!='\0';i++)
+	{
+		len++;
+	}
+	
+	l = len;
+	len--;
+	
+	for(i=0;i<l;i++,len--)
+	{
+		rev[len-1] = name[i];
+	}
+	
+	for(i=0;name[i]!='\0';i++)
+	{
+		if(name[i]==rev[i])
+		{
+			a = 0;
+		}	
+		else
+		{
+			break;
+		}
+			
+	}
+	
+	(a==0)
+		?printf("\n Name Is Palindrome")
+		:printf("\n Name Is Not Palindrome");	
+	
+}
+
